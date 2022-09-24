f69376c34a91bf9455991abbcc284a4f71b7144c
Deepika9103 <102160852+Deepika9103@users.noreply.github.com>
2022-09-25 00:34:10 +0530

    backend task1(python program)

---
+++ task1_be.py
@@ -0,0 +1,21 @@
+def func1(a,b):
+    value={}
+    while(a<b):
+        i=list(bin(a))
+        print(f"the number is {a} the binary value is {i}")
+        for items in range(len(i)-1):
+                if(i[items]=='1' and i[items+1]=='1'):
+                    value.update({a:"true"})
+                    break
+                else:
+                    value.update({a:"false"})
+                    continue
+            
+
+        a=a+1
+    print(value)
+
+print("enter any two numbers which will be the range")
+m=int(input())
+n=int(input())
+func1(m,n)
